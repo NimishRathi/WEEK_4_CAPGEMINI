9d18bfc43ffd2fd6983bfaaac95a120f500a9bad
Nimish Rathi <121229492+NimishRathi@users.noreply.github.com>
2025-02-11 13:29:14 +0530

    "[nimish]add.UniversityCourseManagementTest.java"

--- Test/UniversityCourseManagementTest.java
+++ Test/UniversityCourseManagementTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//add class
 public class UniversityCourseManagementTest {
     private Department<ExamCourse> scienceDepartment;
     private Department<AssignmentCourse> artsDepartment;
